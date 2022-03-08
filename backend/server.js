f2452b91b24cbdbf5438d15bb0ea1f1f7a384c86
Prashant Gohil <prashantgohil248@gmail.com>
2022-03-08 20:48:47 +0530

    basic express app setup in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/api/goals', (req, res) => {
+  res.json({
+    message: 'API working',
+  });
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
