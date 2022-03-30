bdb7733c068ab01ead97dbfc16b1154e7b121526
Prashant Gohil <prashantgohil248@gmail.com>
2022-03-30 23:47:56 +0530

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import goalRouter from './routes/goalRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import { errorHandler } from './middleware/errorMiddleware.js';
 import { connectDB } from './config/DB.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -17,6 +18,17 @@ app.use(express.json());
 
 app.use('/api/goals', goalRouter);
 app.use('/api/users', userRouter);
+
+const __dirname = path.resolve();
+
+// serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+} else {
+  app.get('/', (req, res) => res.send('You are in development mode'));
+}
 
 app.use(errorHandler);
 
