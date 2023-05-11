76151834c739d99afb53e4ce0fe54a3766dce479
Prashant Gohil <prashantgohil248@gmail.com>
2023-05-11 23:20:58 +0530

    Backend: update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,13 +22,11 @@ app.use('/api/users', userRouter);
 const __dirname = path.resolve();
 
 // serve frontend
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
-} else {
-  app.get('/', (req, res) => res.send('You are in development mode'));
-}
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+console.log(__dirname);
+
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')));
 
 app.use(errorHandler);
 
